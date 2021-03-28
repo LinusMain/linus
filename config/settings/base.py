dca7fa336867a2dd6580fa7e078ab9d579dd4091
LinusMain <linusmain@gmain.com>
2021-03-29 00:14:36 +0700

    sentry logging.

--- config/settings/base.py
+++ config/settings/base.py
@@ -261,3 +261,20 @@ SOCIALACCOUNT_ADAPTER = 'linus.users.adapters.SocialAccountAdapter'
 # Your stuff...
 # ------------------------------------------------------------------------------
 
+if not DEBUG:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn="https://30fffe865fb04463a1ea642203cc900e@o217107.ingest.sentry.io/1358518",
+        integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
