eacbbbceae84c2a30fd3045daff49f4e8d3c104b
LinusMain <linusmain@gmain.com>
2021-04-01 22:24:40 +0700

    Sharena.wav probability returned to 1%.

--- linus/static/js/project.js
+++ linus/static/js/project.js
@@ -1,25 +1,32 @@
 /* Project specific Javascript goes here. */
 
+is_sharena_wav_has_been_played_and_user_is_completely_happy = false
+
 playSharenaWav = function () {
   $('body').on('click', function () {
-    var sharenas = [
-      'sharena.wav',
-      'sharena2.wav',
-      'sharena3.wav',
-      'sharena4.wav',
-      'sharena5.wav',
-      'sharena6.wav',
-      'sharena7.wav',
-      'sharena8.wav',
-      'sharena9.wav',
-      'sharena10.wav',
-      'sharena11.wav',
-      'sharena12.wav',
-    ]
-    // if (Math.random() < 0.01) {
-    var to_play = sharenas[Math.floor(Math.random() * sharenas.length)];
-    var audio = new Audio('/static/other/' + to_play);
-    audio.play();
-    // }
+    if (!is_sharena_wav_has_been_played_and_user_is_completely_happy) {
+      is_sharena_wav_has_been_played_and_user_is_completely_happy = true
+      if (Math.random() < 0.01) {
+        var sharenas = [
+          'sharena.wav',
+          'sharena2.wav',
+          'sharena3.wav',
+          'sharena4.wav',
+          'sharena5.wav',
+          'sharena6.wav',
+          'sharena7.wav',
+          'sharena8.wav',
+          'sharena9.wav',
+          'sharena10.wav',
+          'sharena11.wav',
+          'sharena12.wav',
+        ]
+        // if (Math.random() < 0.01) {
+        var to_play = sharenas[Math.floor(Math.random() * sharenas.length)];
+        var audio = new Audio('/static/other/' + to_play);
+        audio.play();
+        // }
+      }
+    }
   });
 }
